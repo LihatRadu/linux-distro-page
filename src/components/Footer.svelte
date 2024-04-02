1f7e4694435124d9c5e4771d8d778b8401de2523
Lihat Radu <radulihat@gmail.com>
2024-04-02 19:13:56 +0300

    update footer style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -11,7 +11,8 @@
 
       <a
         href="/"
-        class="mt-8 inline-block rounded-full border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+        class="mt-8 inline-block rounded-full border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500
+        shadow-indigo-900/40 shadow-lg"
       >
         Get Started
       </a>
